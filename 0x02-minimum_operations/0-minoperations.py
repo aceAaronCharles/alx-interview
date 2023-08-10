2a9fe2db9176db53339659806a4f90f8c67acb16
Aaron0Chillz <charlesunlimited10@gmail.com>
2023-08-10 17:07:14 +0300

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,15 +1,20 @@
 #usr/bin/python3
 """minimum number of operations to reach n"""
 def minOperations(n):
-    if n == 1:
+
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
         return 0
-    operations = 0
-    H = 1
-    while H < n:
-        if n % H == 0:
-            operations += 2
-            H *= 2
-        else:
-            H += H
-            operations += 1
-    return operations if H == n else 0
+    ops, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            ops += root
+            # set n to the remainder
+            n = n / root
+            # reduce root to find remaining smaller vals that evenly-divide n
+            root -= 1
+        # increment root until it evenly-divides n
+        root += 1
+    return ops
