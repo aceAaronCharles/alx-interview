9ceb46f568f9eb280c558b8f5de8f31efdd92c15
Aaron0Chillz <charlesunlimited10@gmail.com>
2023-08-24 13:11:36 +0300

    method that determines if a given data set represents a valid UTF-8 encoding

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -35,14 +35,3 @@ def validUTF8(data):
                 return False
     
     return num_bytes_to_follow == 0
-
-
-if __name__ == "__main__":
-    data = [65]
-    print(validUTF8(data))
-
-    data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-    print(validUTF8(data))
-
-    data = [229, 65, 127, 256]
-    print(validUTF8(data))
