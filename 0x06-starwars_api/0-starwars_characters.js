f44feac2b1a799901d8e6ecc1a217c43bd84a973
Aaron0Chillz <charlesunlimited10@gmail.com>
2023-09-07 13:53:50 +0300

    a script that prints all characters of a Star Wars movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,50 +1,17 @@
 #!/usr/bin/node
 
-// Import necessary packages
 const request = require('request');
 
-// Check if a Movie ID is provided as a command-line argument
-if (process.argv.length !== 3) {
-  console.error('Usage: ./starWarsCharacters.js <Movie ID>');
-  process.exit(1);
-}
-
-// Get the Movie ID from command-line arguments
-const movieId = process.argv[2];
-
-// Define the URL to fetch movie data from the Star Wars API
-const url = `https://swapi.dev/api/films/${movieId}/`;
-
-// Make a GET request to the API
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Error:', response.statusMessage);
-    process.exit(1);
-  }
-
-  // Parse the JSON response
-  const movieData = JSON.parse(body);
-
-  // Extract and print character names
-  movieData.characters.forEach((characterUrl) => {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error:', charError);
-        process.exit(1);
-      }
-
-      if (charResponse.statusCode !== 200) {
-        console.error('Error:', charResponse.statusMessage);
-        process.exit(1);
-      }
-
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name);
-    });
-  });
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
