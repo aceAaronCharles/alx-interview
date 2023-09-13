bdc75b1b374b280ff3735b8fe484ed71d3015ab3
Aaron0Chillz <charlesunlimited10@gmail.com>
2023-09-13 13:43:11 +0300

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,9 +2,9 @@
 
 """
 Rotate Image
-You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
-You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
 """
+
+
 def rotate_2d_matrix(matrix):
     '''rotates a 2d matrix 90° clockwise
     Returns: Nothing'''
@@ -13,7 +13,7 @@ def rotate_2d_matrix(matrix):
     while left < right:
         for i in range(right - left):
             top, bottom = left, right
-            # save topleft  value
+            # save topleft value
             topLeft = matrix[top][left + i]
             # move bottom left to top left
             matrix[top][left + i] = matrix[bottom - i][left]
